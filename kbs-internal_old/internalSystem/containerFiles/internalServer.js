9e9bc3c3c4cb15dee170b359017cab3855e29444
Nawaf  Alharbi <nawaf.manage@gmail.com>
2021-04-10 00:54:25 +0300

    update

--- kbs-internal_old/internalSystem/containerFiles/internalServer.js
+++ kbs-internal_old/internalSystem/containerFiles/internalServer.js
@@ -1,3 +1,32 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('../../_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.use('/home', require('./public/home'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
+
+
+
+
+
+
+/*
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -87,3 +116,4 @@ router.route("/downloadTerms")
         res.setHeader('Content-Disposition', 'attachment; filename=agreement.pdf');
         file.pipe(res);
     })
+*/
